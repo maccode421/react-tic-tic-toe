151adc6099d6ff3f3aafcdd8a675975fcf93e5fc
maccode421 <maccavalli@yahoo.com>
2018-01-26 18:57:54 -0500

    replace Square class with Square function

--- src/index.js
+++ src/index.js
@@ -2,22 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null,
-  //   };
-  // }
+// class Square extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: null,
+//     };
+//   }
 
-  render() {
-    return (
-      <button className="square" onClick={() =>
-        this.props.onClick({ value: 'X' })}>
-        {this.props.value}
-      </button>
-    );
-  }
+//   render() {
+//     return (
+//       <button className="square" onClick={() =>
+//         this.props.onClick({ value: 'X' })}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -29,7 +37,7 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
-    const squres = this.state.squares.slice();
+    const squares = this.state.squares.slice();
     squares[i] = 'X';
     this.setState({ squares: squares });
   }
